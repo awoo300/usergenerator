85d94d21b604f3a8119b382edb5c824374e243bb
awoo300 <awoo300@aol.com>
2017-06-29 17:45:17 +0100

    Add files via upload

---
+++ testusers.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+require 'users'
+
+def Test_CreateUser
+assert_equals('timeStamp@example.com','timeStamp@example.com')
+end
---
+++ users.rb
@@ -0,0 +1,3 @@
+ def generate_user
+ puts 'TO DO'
+ end
