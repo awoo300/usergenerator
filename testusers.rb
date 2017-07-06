c25d11ef22486e3c5ad42713294bd7523a15f4f1
awoo300 <awoo300@aol.com>
2017-07-06 18:07:47 +0100

    Add files via upload

--- testusers.rb
+++ testusers.rb
@@ -1,6 +1,17 @@
 require 'test/unit'
-require 'users'
+require_relative 'users'
 
-def Test_CreateUser
-assert_equals('timeStamp@example.com','timeStamp@example.com')
+class TestUsers < Test::Unit::TestCase
+
+def test_open_file
+    myusers = Users.new
+    myusers.openfile
+    assert(true,"File not created")
+end
+
+
+def test_file_exists
+  assert(File.exist?("user.csv"), "File not available")
+end
+
 end
--- users.rb
+++ users.rb
@@ -1,3 +1,22 @@
- def generate_user
- puts 'TO DO'
+class Users
+
+def openfile
+  userfile = File.new("user.csv", "w")
+
+  if File.exist?(userfile)
+    puts true
+    puts "\n File created"
+  end
+end
+
+
+ def create_user_email
+   useremail = Time.now + '@example.com'
+    x = 0
+    while x < 4
+       puts Time.now +
+       x++
+      usersinfile = x;
  end
+end
+end
