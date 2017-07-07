ff0bd87444122f399d4213bdc5895bdee2d4cda3
awoo300 <awoo300@aol.com>
2017-07-07 17:40:18 +0100

    Add files via upload

--- testusers.rb
+++ testusers.rb
@@ -14,4 +14,11 @@ def test_file_exists
   assert(File.exist?("user.csv"), "File not available")
 end
 
+
+def test_create_email
+    myusers = Users.new
+    email = myusers.create_user_email
+    assert_equal(email, (Time.now) + "example.com")
+end
+
 end
--- users.rb
+++ users.rb
@@ -11,12 +11,8 @@ end
 
 
  def create_user_email
-   useremail = Time.now + '@example.com'
-    x = 0
-    while x < 4
-       puts Time.now +
-       x++
-      usersinfile = x;
- end
+   useremail = (Time.now)#{"@example.com"}
 end
+
+
 end
